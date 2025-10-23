6ba74c754ca37f87cd2323b785c394b67a9c3129
saintsauceee <t.donghao.zeng@gmail.com>
2025-10-23 14:12:02 -0400

    feat: thomas method and diagonal dominance

--- linear_system_solver.py
+++ linear_system_solver.py
@@ -29,6 +29,11 @@ class LinearSystemSolver:
             P = P_n-1 ... P_2 P_1 (P_k is I_n with rows k, q swapped)
             L = n x n unit lower triangular matrix (elements are the permuted multipliers)
             U = n x n upper triangular matrix obtained after forward elimination
+
+            GEPP is basically the numerically stable version of GENP.
+
+            We can find a (n x n) matrix E that satisfies (A + E)x_c = b and usually norm(E) ~ eps * norm(A).
+            Thus, x_c (computed x) is basically close to x in their vector space (exact sln of a nearby problem).
         """
         pass
 
@@ -58,3 +63,37 @@ class LinearSystemSolver:
         """
         
         pass
+
+    def thomas(self, a, d, c, b, inplace=True):
+        """ Solve a tridiagonal linear system. """
+
+        n = len(d)
+
+        # forward
+        for k in range(n - 1):
+            mult = a[k] / d[k]
+            d[k + 1] -= a[k] * c[k]
+            b[k + 1] -= a[k] * b[k]
+
+        # back
+        x = [0.0] * n
+        x[-1] = b[-1] / d[-1]
+        for k in range(n - 2, -1, -1):
+            x[k] = (b[k] - c[k]* x[k + 1]) / d[k]
+        
+        return x
+    
+    def diag_dominant(self, A, by="col", tol=0.0):
+        """ Check if (n x n) matrix A is diagonally dominant by column. """
+
+        n = len(A)
+        for i in range(n):
+            diag = A[i][i]
+            if by == "row":
+                off = sum(abs(A[i][j]) for j in range(n) if j != i)
+            elif by == "col":
+                off = sum(abs(A[j][i]) for j in range(n) if j != i)
+            if not (diag + tol) >= off:
+                return False
+        
+        return True
