0995a22f50fbbecf4d222af9d373b63398324f4c
saintsauceee <t.donghao.zeng@gmail.com>
2025-10-29 23:35:14 -0400

    add numerical approx to namespace

---
+++ src/num_approx/num_approx.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <cmath>
+#include <stdexcept>
+
+namespace math::num_approx {
+    using Fn = double (*)(double);
+    
+    double secant_slope(Fn fn, double x, double h, bool central_diff=true);
+    double linear_approx(Fn fn, double x, double h);
+    double nth_derivative(double (*fn)(double), double x, double h, int n);
+    double taylor_approx(double (*fn)(double), double x, double h, int n);
+}
